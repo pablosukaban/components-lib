0844c2f364bcf36a045a83c52f5ee12e51f7f9b2
ilya <zapaska96@gmail.com>
2022-10-22 09:26:34 +0500

    first

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { MagnetButton } from './MagnetButton';
+
+export const App = () => {
+    return (
+        <div className="container">
+            <MagnetButton />
+        </div>
+    );
+};
